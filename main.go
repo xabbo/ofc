e346bc4df8d01fc84e326c6ca528d2722ded6275
rob <rob@b7c.io>
2024-07-02 18:18:50 +1200

    move types into main.go

--- main.go
+++ main.go
@@ -26,6 +26,22 @@ const (
 	originsFigureDataUrl  = "http://origins-gamedata.habbo.com/figuredata/1"
 )
 
+type FigureData struct {
+	M, F map[string]FigurePartSets
+}
+
+type FigurePartSets []FigurePartSet
+
+type FigurePartSet struct {
+	Type   string            `json:"-"`
+	Id     int               `json:"s"`
+	Parts  map[string]string `json:"p"`
+	Colors []string          `json:"c"`
+}
+
+// Reverse color map from Figure Part Type -> Color (lowercase hex) -> Modern Color ID
+type ColorMap = map[string]map[string]int
+
 func main() {
 	if err := run(os.Args[1:]); err != nil {
 		if errors.Is(err, errUsage) {
@@ -193,4 +209,31 @@ func loadOriginsFigureData() (fd FigureData, err error) {
 	}
 	err = json.Unmarshal(b, &fd)
 	return
+}
+
+// Fixes the old figure data to valid JSON
+func fixOriginsFigureData(b []byte) {
+	sp := -1
+	stack := [8]struct {
+		i      int
+		object bool
+	}{}
+
+	for i := range b {
+		// assuming these characters don't appear inside any strings
+		switch b[i] {
+		case '[':
+			sp++
+			stack[sp].i = i
+			stack[sp].object = false
+		case ':':
+			stack[sp].object = true
+		case ']':
+			if stack[sp].object {
+				b[stack[sp].i] = '{'
+				b[i] = '}'
+			}
+			sp--
+		}
+	}
 }
